47d82c29fcaa08f45f39ba0f3326e33da220e079
kriash03 <97736931+kriash03@users.noreply.github.com>
2022-11-09 19:18:03 +0530

    Initial Commit

---
+++ musicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicApp
+//
+//  Created by krishna on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
